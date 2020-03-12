535c43e1eed096efd6fdaf6935f7d28f2ef5ff96
panosmania <panosmania@users.noreply.github.com>
2020-03-12 15:00:23 +0200

    broken our homepage into smaller components, finished styling our menu-item component

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 
 import "./homepage.styles.scss";
+import Directory from "../../components/directory/directory.component";
 
 const Homepage = props => (
   <div className="homepage">
+    <Directory />
+  </div>
+);
+
+export default Homepage;
+
+/* 
+<div className="homepage">
     <div className="directory-menu">
       <div className="menu-item">
         <div className="content">
@@ -37,6 +46,4 @@ const Homepage = props => (
       </div>
     </div>
   </div>
-);
-
-export default Homepage;
+*/
